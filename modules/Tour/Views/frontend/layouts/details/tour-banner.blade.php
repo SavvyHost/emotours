edf8ee11c66173a12efbd49f86fece3841202223
ojpro <contact@ojpro.me>
2022-07-29 12:07:19 +0100

    edit tour banner button, (still need to show the : #4)

--- modules/Tour/Views/frontend/layouts/details/tour-banner.blade.php
+++ modules/Tour/Views/frontend/layouts/details/tour-banner.blade.php
@@ -1,12 +1,20 @@
 @if($row->banner_image_id)
     <div class="bravo_banner">
-        <section class="hero_in tours_detail" style="background: url({{$row->getBannerImageUrlAttribute('full')}}) center center no-repeat">
+        <section class="hero_in tours_detail"
+                 style="background: url({{$row->getBannerImageUrlAttribute('full')}}) center center no-repeat">
             <div class="wrapper">
                 <div class="container">
                     <h1 class="fadeInUp text-center"><span></span> {{ clean($translation->title) }} </h1>
                 </div>
                 <span class="magnific-gallery">
-					<a href="{{$row->getBannerImageUrlAttribute('full')}}" class="btn_photos" title="Photo title" data-effect="mfp-zoom-in">{{ __('View photos') }}</a>
+					<a href="{{$row->getBannerImageUrlAttribute('full')}}" class="btn_photos" title="Photo title"
+                       data-effect="mfp-zoom-in">
+                         @if($row->video)
+                            {{ __('View Video') }}
+                        @else
+                            {{ __('View Photos') }}
+                        @endif
+                        </a>
                 </span>
             </div>
         </section>
