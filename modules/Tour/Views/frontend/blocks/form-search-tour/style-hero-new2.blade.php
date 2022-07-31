2915140b5d4666864cf3e6f1f52528c1667c5fbf
abooda sayed <aboodasayed25@gmail.com>
2022-07-31 23:06:53 +0200

    added new seach form fields , need complete fields

--- modules/Tour/Views/frontend/blocks/form-search-tour/style-hero-new2.blade.php
+++ modules/Tour/Views/frontend/blocks/form-search-tour/style-hero-new2.blade.php
@@ -5,42 +5,7 @@
 					<div class="main_search">
 						<h3>Book Unique Experiences</h3>
 						<p>top-rated tours around Egypt!</p>
-						<form>
-							<div class="custom-search-input-2">
-								<div class="form-group">
-									<input class="form-control" type="text" placeholder="Where..." id="autocomplete">
-									<i class="icon_pin_alt"></i>
-								</div>
-								<div class="form-group">
-									<input class="form-control" type="text" name="dates" placeholder="When..">
-									<i class="icon_calendar"></i>
-								</div>
-								<div class="row">
-									<div class="col-8">
-										<div class="panel-dropdown">
-											<a href="#">Guests <span class="qtyTotal">1</span></a>
-											<div class="panel-dropdown-content">
-												<!-- Quantity Buttons -->
-												<div class="qtyButtons">
-													<label>Adults</label>
-													<input type="text" name="qtyInput" value="1">
-												</div>
-												<div class="qtyButtons">
-													<label>Childrens</label>
-													<input type="text" name="qtyInput" value="0">
-												</div>
-											</div>
-										</div>
-									</div>
-									<div class="col-4">
-										<div class="form-group">
-											<input type="submit" class="btn_search" value="Search">
-										</div>
-									</div>	
-								</div>
-							</div>
-							<!-- /row -->
-						</form>
+						@include('Tour::frontend.layouts.search.form-search-new')
 					</div>
 					<!-- /main_search -->	
 				</div>
