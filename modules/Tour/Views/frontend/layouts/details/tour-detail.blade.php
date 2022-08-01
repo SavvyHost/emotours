cdeae6f04ea62e9f1c75019a979a165da6c3f799
ojpro <contact@ojpro.me>
2022-08-01 11:48:58 +0100

    added tour's gallery

--- modules/Tour/Views/frontend/layouts/details/tour-detail.blade.php
+++ modules/Tour/Views/frontend/layouts/details/tour-detail.blade.php
@@ -75,7 +75,41 @@
                 </div>
             @endif
 
-            <section id="description" class="g-overview">
+
+                @if($row->getGallery())
+                    <div class="g-gallery">
+                        <div class="fotorama" data-width="100%" data-thumbwidth="135" data-thumbheight="135" data-thumbmargin="15" data-nav="thumbs" data-allowfullscreen="true">
+                            @foreach($row->getGallery() as $key=>$item)
+                                <a href="{{$item['large']}}" data-thumb="{{$item['thumb']}}" data-alt="{{ __("Gallery") }}"></a>
+                            @endforeach
+                        </div>
+                        <div class="social">
+                            <div class="social-share">
+                <span class="social-icon">
+                    <i class="icofont-share"></i>
+                </span>
+                                <ul class="share-wrapper">
+                                    <li>
+                                        <a class="facebook" href="https://www.facebook.com/sharer/sharer.php?u={{$row->getDetailUrl()}}&amp;title={{$translation->title}}" target="_blank" rel="noopener" original-title="{{__("Facebook")}}">
+                                            <i class="fa fa-facebook fa-lg"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a class="twitter" href="https://twitter.com/share?url={{$row->getDetailUrl()}}&amp;title={{$translation->title}}" target="_blank" rel="noopener" original-title="{{__("Twitter")}}">
+                                            <i class="fa fa-twitter fa-lg"></i>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="service-wishlist {{$row->isWishList()}}" data-id="{{$row->id}}" data-type="{{$row->type}}">
+                                <i class="fa fa-heart-o"></i>
+                            </div>
+                        </div>
+                    </div>
+                @endif
+
+
+                <section id="description" class="g-overview">
                 <h2>{{__("Overview")}}</h2>
                 <div class="description">
                     @php echo $translation->content @endphp
