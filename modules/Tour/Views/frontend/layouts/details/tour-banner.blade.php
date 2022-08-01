913f78168c3d7808616d3c44de6513b94c5cf493
ojpro <contact@ojpro.me>
2022-08-01 01:07:01 +0100

    fix tour video modal

--- modules/Tour/Views/frontend/layouts/details/tour-banner.blade.php
+++ modules/Tour/Views/frontend/layouts/details/tour-banner.blade.php
@@ -6,18 +6,33 @@
                 <div class="container">
                     <h1 class="fadeInUp text-center"><span></span> {{ clean($translation->title) }} </h1>
                 </div>
-                <span class="magnific-gallery">
-					<a href="{{$row->getBannerImageUrlAttribute('full')}}" class="btn_photos" title="Photo title"
-                       data-effect="mfp-zoom-in">
-                         @if($row->video)
-                            {{ __('View Video') }}
-                        @else
-                            {{ __('View Photos') }}
-                        @endif
-                        </a>
-                </span>
+                @if($row->video)
+                    <span>
+                        <a href="{{$row->getBannerImageUrlAttribute('full')}}" class="btn_photos" title="Photo title"
+                           data-effect="mfp-zoom-in" data-toggle="modal" data-src="{{ handleVideoUrl($row->video) }}"
+                           data-target="#myModal">
+                                {{ __('Tour Video') }}
+                            </a>
+                    </span>
+                @endif
             </div>
         </section>
+        <div class="modal fade z-max" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+             aria-hidden="true">
+            <div class="modal-dialog modal-lg" role="document">
+                <div class="modal-content">
+                    <div class="modal-body">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <iframe class="embed-responsive-item bravo_embed_video" src="{{ handleVideoUrl($row->video) }}" allowscriptaccess="always"
+                                    allow="autoplay"></iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endif
 
