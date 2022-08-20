22f903b2e86ffe158458acb904c866d5eb2e1015
abooda sayed <aboodasayed25@gmail.com>
2022-08-21 01:23:26 +0200

    just commit

--- modules/Tour/Views/frontend/search.blade.php
+++ modules/Tour/Views/frontend/search.blade.php
@@ -9,7 +9,7 @@
    <div class="bravo_search_tour">
         <section class="hero_in tours" @if($bg = setting_item("tour_page_search_banner")) style="background-image: url({{get_file_url($bg,'full')}})" @endif>
 				<div class="wrapper">
-					<div class="container">
+					<div class="container-fluid">
 						<h1 class="fadeInUp"><span></span>{{setting_item_with_lang("tour_page_search_title")}}</h1>
 					</div>
 				</div>
